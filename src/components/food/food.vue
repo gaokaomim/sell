819560adc08932169fc3517109a109d4ac618ab2
gaokaomim <gaokaomim@163.com>
2017-07-18 16:57:48 +0800

    新增商品详情和商店评论

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -21,6 +21,38 @@
                     <div class="cartcontrol-wrapper">
                         <cartControl @add="addFood" :food="food"></cartControl>
                     </div>
+                    <transition name="fade">
+                        <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">
+                            加入购物车
+                        </div>
+                    </transition>
+                </div>
+                <split v-show="food.info"></split>
+                <div class="info" v-show="food.info">
+                    <h1 class="title">商品信息</h1>
+                    <p class="text">{{food.info}}</p>
+                </div>
+                <split></split>
+                <div class="rating">
+                    <h1 class="title">商品评价</h1>
+                    <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings">
+    
+                    </ratingselect>
+                    <div class="rating-wrapper">
+                        <ul v-show="food.ratings && food.ratings.length">
+                            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+                                <div class="user">
+                                    <span class="name">{{rating.username}}</span>
+                                    <img class="avatar" width="12" height="12" :src="rating.avatar">
+                                </div>
+                                <div class="time">{{rating.rateTime | formatDate}}</div>
+                                <p class="text">
+                                    <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,8 +63,8 @@ import BScroll from 'better-scroll';
 import Vue from 'vue';
 import { formatDate } from 'common/js/date';
 import cartControl from 'components/cartControl/cartControl';
-// import ratingselect from 'components/ratingselect/ratingselect';
-// import split from 'components/split/split';
+import ratingselect from 'components/ratingselect/ratingselect';
+import split from 'components/split/split';
 const ALL = 2;
 export default {
     props: {
@@ -110,13 +142,16 @@ export default {
         }
     },
     components: {
-        cartControl
+        cartControl,
+        split,
+        ratingselect
     }
 
 };
 </script>
-<style lang="stylus" rel="stylesheet/stylus">
+ <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
+
   .food
     position: fixed
     left: 0
@@ -150,6 +185,7 @@ export default {
           padding: 10px
           font-size: 20px
           color: #fff
+
     .content
       position: relative
       padding: 18px
@@ -259,6 +295,7 @@ export default {
               color: rgb(0, 160, 220)
             .icon-thumb_down
               color: rgb(147, 153, 159)
+
         .no-rating
           padding: 16px 0
           font-size: 12px
